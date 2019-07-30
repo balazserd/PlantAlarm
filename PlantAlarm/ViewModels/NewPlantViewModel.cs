64c372bd5e1d5712bdc282bdf13bccaab4639474
balazserdesz <balazs.erdesz@stud.uni-corvinus.hu>
2019-07-30 20:37:35 +0200

    photo delete/change implemented

--- PlantAlarm/ViewModels/NewPlantViewModel.cs
+++ PlantAlarm/ViewModels/NewPlantViewModel.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using PlantAlarm.DatabaseModels;
 using PlantAlarm.Services;
@@ -48,8 +49,11 @@ namespace PlantAlarm.ViewModels
 
         public ICommand ShowCategorySelectorPageCommand { get; private set; }
         public ICommand AddPhotoCommand { get; set; }
+        public ICommand ShowPhotoOptionsCommand { get; set; }
+        public ICommand DeletePhotoCommand { get; set; }
+        public ICommand ChangePhotoCommand { get; set; }
 
-        public ObservableCollection<PlantPhoto> Photos { get; private set; }
+        public ObservableCollection<PlantPhotoItem> Photos { get; private set; }
 
         public NewPlantViewModel(INavigation navigation, Page viewForViewModel)
         {
@@ -62,47 +66,9 @@ namespace PlantAlarm.ViewModels
             });
             AddPhotoCommand = new Command(async () =>
             {
-                if (PlantToAdd == null)
-                {
-                    //Add the plant instantly, so that we will have an id for the plant photos.
-                    PlantToAdd = new Plant { Name = "Placeholder Name" };
-                    await PlantService.AddPlantAsync(PlantToAdd);
-                }
+                await InitPlantToAddIfNeeded();
 
-                //Create the actions for the add photo dialog.
-                var actions = new List<string>();
-                
-                if (CrossMedia.Current.IsCameraAvailable && CrossMedia.Current.IsTakePhotoSupported) actions.Add("Take photo");
-                if (CrossMedia.Current.IsPickPhotoSupported) actions.Add("Pick from device");
-
-                string actionToTake = await View.DisplayActionSheet("Select an option", "Back", null, actions.ToArray());
-
-                MediaFile image;
-                switch (actionToTake)
-                {
-                    case "Take photo":
-                        {
-                            image = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
-                            {
-                                PhotoSize = PhotoSize.Medium,
-                                CompressionQuality = 75,
-                                SaveToAlbum = true
-                            });
-                            break;
-                        }
-                    case "Pick from device":
-                        {
-                            image = await CrossMedia.Current.PickPhotoAsync(new PickMediaOptions
-                            {
-                                PhotoSize = PhotoSize.Medium,
-                                CompressionQuality = 75,
-                            });
-                            break;
-                        }
-                    case "Back": return;
-                    default: throw new InvalidOperationException();
-                }
-
+                var image = await GetNewPhoto();
                 if (image == null) return;
 
                 //Save the created image to the local folder. Must be done, or it can be removed by the user from their public picture folder.
@@ -111,25 +77,116 @@ namespace PlantAlarm.ViewModels
 
                 await image.GetStreamWithImageRotatedForExternalStorage().CopyToAsync(File.Create(fullUrl));
 
-                var photo = new PlantPhoto
-                {
-                    PlantFk = PlantToAdd.Id,
-                    IsPrimary = false,
-                    TakenAt = DateTime.Now,
-                    Url = fullUrl
-                };
-
+                var photoItem = new PlantPhotoItem(
+                    new PlantPhoto
+                    {
+                        PlantFk = PlantToAdd.Id,
+                        IsPrimary = false,
+                        TakenAt = DateTime.Now,
+                        Url = fullUrl
+                    },
+                    this.ShowPhotoOptionsCommand);
+                
                 //Add the created object to the collection of photos.
-                Photos.Add(photo);
+                Photos.Add(photoItem);
+            });
+            ShowPhotoOptionsCommand = new Command(async (ppi) =>
+            {
+                string action = await View.DisplayActionSheet("Select an option", "Cancel", "Delete photo", "Change photo");
+
+                var plantPhotoItem = ppi as PlantPhotoItem;
+                switch (action)
+                {
+                    case "Delete photo":
+                        DeletePhotoCommand.Execute(plantPhotoItem);
+                        break;
+                    case "Change photo":
+                        ChangePhotoCommand.Execute(plantPhotoItem);
+                        break;
+                    case "Cancel":
+                        break;
+                    default: throw new InvalidOperationException("Unknown action chosen in ActionSheet.");
+                }
+            });
+            DeletePhotoCommand = new Command(async (ppi) =>
+            {
+                var plantPhotoItem = ppi as PlantPhotoItem;
+
+                File.Delete(plantPhotoItem.Photo.Url);
+                Photos.Remove(plantPhotoItem);
+            });
+            ChangePhotoCommand = new Command(async (ppi) =>
+            {
+                var image = await GetNewPhoto();
+                if (image == null) return;
+
+                var plantPhotoItem = ppi as PlantPhotoItem;
+                var index = Photos.IndexOf(plantPhotoItem);
+
+                File.Delete(plantPhotoItem.Photo.Url);
+                await image.GetStreamWithImageRotatedForExternalStorage().CopyToAsync(File.Create(plantPhotoItem.Photo.Url));
+
+                plantPhotoItem.Photo.TakenAt = DateTime.Now;
+                Photos.Remove(plantPhotoItem);
+                Photos.Insert(index, plantPhotoItem);
             });
 
-            MessagingCenter.Subscribe<object, List<PlantCategory>>(this, "CategoriesSelected", async (viewModel, categoryList) =>
+            MessagingCenter.Subscribe<object, List<PlantCategory>>(this, "CategoriesSelected", (viewModel, categoryList) =>
             {
                 Categories = categoryList;
             });
 
             Categories = new List<PlantCategory>();
-            Photos = new ObservableCollection<PlantPhoto>();
+            Photos = new ObservableCollection<PlantPhotoItem>();
+        }
+
+        private async Task<MediaFile> GetNewPhoto()
+        {
+            //Create the actions for the add photo dialog.
+            var actions = new List<string>();
+
+            if (CrossMedia.Current.IsCameraAvailable && CrossMedia.Current.IsTakePhotoSupported) actions.Add("Take photo");
+            if (CrossMedia.Current.IsPickPhotoSupported) actions.Add("Pick from device");
+
+            string actionToTake = await View.DisplayActionSheet("Select an option", "Back", null, actions.ToArray());
+
+            MediaFile image;
+            switch (actionToTake)
+            {
+                case "Take photo":
+                    {
+                        image = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                        {
+                            PhotoSize = PhotoSize.Medium,
+                            CompressionQuality = 75,
+                            SaveToAlbum = true
+                        });
+                        break;
+                    }
+                case "Pick from device":
+                    {
+                        image = await CrossMedia.Current.PickPhotoAsync(new PickMediaOptions
+                        {
+                            PhotoSize = PhotoSize.Medium,
+                            CompressionQuality = 75,
+                        });
+                        break;
+                    }
+                case "Back": return null;
+                default: throw new InvalidOperationException("Unknown action chosen in ActionSheet.");
+            }
+
+            return image;
+        }
+
+        private async Task InitPlantToAddIfNeeded()
+        {
+            if (PlantToAdd == null)
+            {
+                //Add the plant instantly, so that we will have an id for the plant photos.
+                PlantToAdd = new Plant { Name = "Placeholder Name" };
+                await PlantService.AddPlantAsync(PlantToAdd);
+            }
         }
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
@@ -138,5 +195,20 @@ namespace PlantAlarm.ViewModels
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public class PlantPhotoItem
+    {
+        public PlantPhoto Photo { get; set; }
+        public ICommand ShowOptions { get; set; }
+
+        public PlantPhotoItem(PlantPhoto plantPhoto, ICommand ShowOptionsCommand)
+        {
+            Photo = plantPhoto;
+            ShowOptions = new Command(() =>
+            {
+                ShowOptionsCommand.Execute(this);
+            });
+        }
     }
 }
